575538391bc2face4d925413188fee76c1c89d70
Turkayildiz <Turkayefeyildiz@gmail.com>
2025-02-16 16:26:39 +0300

    bata2

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-
-document.getElementById('submit').addEventListener('click', function(event) {
-
-    event.preventDefault();
-
-    // Form verilerini al
-    const ad = document.getElementById('ad').value;
-    const soyad = document.getElementById('soyad').value;
-    const tarih = document.getElementById('tarih').value;
-    const email = document.getElementById('email').value;
-    const TC = document.getElementById('TC').value;
+function kaydet()
+{
+    let kayit= localStorage.getItem("kayit");
+    let myObj = JSON.parse(kayit);
+    console.log(kayit);
+    console.log(myObj);
+    let ad = document.getElementById('ad').value;
+    let soyad = document.getElementById('soyad').value;
+    let tarih = document.getElementById('tarih').value;
+    let email = document.getElementById('email').value;
+    let TC = document.getElementById('TC').value;
+    let sonuc = document.getElementById('sonuc').value;
+    ad.value=myObj.ad;
+    soyad.value=myObj.soyad;
+    tarih.value=myObj.tarih;
+    email.value=myObj.email;
+    TC.value=myObj.TC;
+    sonuc.value=myObj.sonuc;
 
     // Verileri bir obje olarak oluştur
-    const hastaVerisi = {
-        ad: ad,
-        soyad: soyad,
-        tarih: tarih,
-        email: email,
-        TC: TC
+    var kayıt = {
+        ad: ad.value,
+        soyad: soyad.value,
+        tarih: tarih.value,
+        email: email.value,
+        TC: TC.value
     };
 
-    // Veriyi JSON formatında localStorage'a kaydet
-    localStorage.setItem('hastaVerisi', JSON.stringify(hastaVerisi));
-
-    console.log('Hasta Verisi Kaydedildi:', hastaVerisi);
-
+    console.log(kayıt);
+    localStorage.setItem("kayit", JSON.stringify(kayıt));
     alert('Hasta verisi başarıyla kaydedildi!');
-});
+    };
