c88214a419defa86da7f080cd10ee6c031f18ae6
Turkayildiz <Turkayefeyildiz@gmail.com>
2025-02-16 16:26:07 +0300

    bata1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-document.getElementById('kaydetBtn').addEventListener('click', function(event) {
+document.getElementById('submit').addEventListener('click', function(event) {
 
     event.preventDefault();
 
